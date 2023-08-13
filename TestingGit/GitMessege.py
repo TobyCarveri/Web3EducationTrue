511e4501ab83db4fb0d2ed60f7aa2458713d82c3
TobyCarvery <71832701+TobyCarveri@users.noreply.github.com>
2023-08-13 17:39:09 +0200

    Update GitMessege.py
    Testing change something on github and send it to pc

--- TestingGit/GitMessege.py
+++ TestingGit/GitMessege.py
@@ -1,1 +1,2 @@
 Print('I make now correct fork and litle bit more understand git. Now i send this file to TestingGit and try commit it and push to GITHUB')
+print('Now i also know i can add folder only if i make there a file or send actual P.S i want git pull it on computer')
